51d737d45d455f7dd0d87210459ddc02cd525d65
Tamiris Alves <tamialves_67@hotmail.com>
2025-09-13 19:43:09 -0400

    Implementação do MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,37 @@
+package application;
+
 import javafx.application.Application;
-import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
 
 
 public class Main extends Application{
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+            Scene mainScene = new Scene(scrollPane);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
 
 
-void main() {
-}
