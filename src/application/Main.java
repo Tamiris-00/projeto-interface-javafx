34e1d2d9c9f0d234379208a0a1ea3478b095ab39
Tamiris Alves <tamialves_67@hotmail.com>
2025-09-12 21:01:17 -0400

    Primeiro commit

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+}
+
+
+void main() {
+}
