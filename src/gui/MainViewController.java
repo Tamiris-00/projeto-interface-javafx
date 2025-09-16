760189f8195018faa164b723bf791a5fb39e53a3
Tamiris Alves <tamialves_67@hotmail.com>
2025-09-15 22:17:48 -0400

    Mudanças no MainViewController

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -2,6 +2,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -13,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
@@ -23,6 +25,8 @@ public class MainViewController implements Initializable {
     private MenuItem menuItemDepartment;
     @FXML
     private MenuItem menuItemAbout;
+    @FXML
+    private VBox mainVbox;
 
     @FXML
     public void onMenuItemSellerAction(){
@@ -47,20 +51,28 @@ public class MainViewController implements Initializable {
 
     }
 
-    private synchronized void loadView(String absoluteName) {
+
+
+    private synchronized<T> void loadView(String absoluteName, Consumer<T> initializingAction){
+
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-            VBox newVbox = loader.load();
+            VBox newBox = loader.load();
 
-            Scene mainScene = Main.getMainScene();
-            VBox mainVbox =(VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+            if (mainVbox.getChildren().size() > 1) {
+                mainVbox.getChildren().remove(1, mainVbox.getChildren().size());
+            }
+            mainVbox.getChildren().addAll(newBox.getChildren());
 
-            Node mainMenu = mainVbox.getChildren().getFirst();
-            mainVbox.getChildren().clear();
-            mainVbox.getChildren().addAll(newVbox.getChildren());
+            T controller = loader.getController();
+            initializingAction.accept(controller);
 
         }catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    private synchronized <T> void loadView(String absoluteName) {
+        loadView(absoluteName, x -> {});
     }
 }
