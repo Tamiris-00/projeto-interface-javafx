6a80a1ff4b8232f0a8d8406316402363d52e33a3
Tamiris Alves <tamialves_67@hotmail.com>
2025-09-22 21:35:40 -0400

    Saving new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,20 +1,26 @@
 package gui;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
-import java.io.Serializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -30,14 +36,37 @@ public class DepartmentFormController implements Initializable {
     public void setDepartment(Department entity)  {
         this.entity = entity;
     }
+    public void setDepartmentService(DepartmentService service){
+        this.service = service;
+    }
 
     @FXML
-    public void onBtSaveAction(){
-        System.out.println("save");
+    public void onBtSaveAction(ActionEvent event){
+        if(entity ==null){
+            throw new IllegalStateException("Entity was null");
+        }
+        if(service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        try{
+        entity = getFormData();
+        service.saveOrUpdate(entity);
+        Utils.currentStage(event).close();
+        }
+        catch (DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
+
+    private Department getFormData() {
+       Integer id = Utils.tryParseToInt(txtId.getText());
+       String name = txtName.getText();
+       return new Department(id, name);
+    }
+
     @FXML
-    public void onBtCancelAction(){
-        System.out.println("cancel");
+    public void onBtCancelAction(ActionEvent event){
+        Utils.currentStage(event).close();
     }
 
 
